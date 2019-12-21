7d12de1ed4741f913ac36b18f8c4993b392b2fa7
Oleg Andreych <kjiec4@gmail.com>
2019-12-21 13:56:36 +0500

    fixup! fixup! fixup! fixup! fixup! Adding some styles.

--- src/main/kotlin/org/andreych/tpe_champions/configuration/mvc/WebMvcConfigurer.kt
+++ src/main/kotlin/org/andreych/tpe_champions/configuration/mvc/WebMvcConfigurer.kt
@@ -3,7 +3,7 @@ package org.andreych.tpe_champions.configuration.mvc
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 /**
  * Configures some knobs in MVC part.
@@ -11,10 +11,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * Created by oleg on 30.05.17.
  */
 @Configuration
-open class WebMvcConfigurer @Autowired constructor(private val noCacheInterceptor: NoCacheInterceptor) : WebMvcConfigurerAdapter()
-{
-    override fun addInterceptors(registry: InterceptorRegistry?)
-    {
+open class WebMvcConfigurer @Autowired constructor(private val noCacheInterceptor: NoCacheInterceptor) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry?) {
         registry?.addInterceptor(noCacheInterceptor)
     }
 }
